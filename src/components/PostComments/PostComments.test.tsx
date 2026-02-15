8ee47757281fcbcba8eaa57facdbd3f189a6825b
Lucas Calíope <lucascaliope2010@gmail.com>
2026-02-14 23:24:03 -0300

    atributo_data_testid
    Added tests to verify comment insertion functionality.

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,35 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  it("Deve permitir a inserção de dois comentários", () => {
+    render(<PostComment />);
+
+    
+    fireEvent.change(screen.getByTestId("comment-textarea"), {
+      target: {
+        value: "Primeiro comentário",
+      },
     });
+    fireEvent.click(screen.getByTestId("comment-button"));
+
+    
+    fireEvent.change(screen.getByTestId("comment-textarea"), {
+      target: {
+        value: "Segundo comentário",
+      },
+    });
+    fireEvent.click(screen.getByTestId("comment-button"));
+
+   
+    const comments = screen.getAllByTestId("comment-element");
+    expect(comments).toHaveLength(2);
+    expect(screen.getByText("Primeiro comentário")).toBeInTheDocument();
+    expect(screen.getByText("Segundo comentário")).toBeInTheDocument();
+  });
 });
